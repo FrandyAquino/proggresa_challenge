10c16ae07ca21fdd99d95c78ec2f719afca52624
FrandyAquino <frandyjavieraquino13@gmail.com>
2024-12-05 23:47:26 -0400

    Actualizar la configuración de Vercel para apuntar a src/app.ts y corregir importaciones en middleware

--- backend/src/shared/middlewares/schema.ts
+++ backend/src/shared/middlewares/schema.ts
@@ -1,4 +1,4 @@
-import { NextFunction,Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { ZodSchema } from 'zod'
 import { errorZodResponse } from '../functions/response'
 
